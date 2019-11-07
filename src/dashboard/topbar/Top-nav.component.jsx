96121d677e2d7ae303801bdb3e4c56ab5048891f
SeemranXec <bleh17xec@hotmail.com>
2019-11-07 13:16:49 +0530

    styling top-bar

--- src/dashboard/topbar/Top-nav.component.jsx
+++ src/dashboard/topbar/Top-nav.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import userImage from '../../assets/user-image.jpg';
 class TopNavBarComponent extends React.Component {
     constructor(){
         super();
         this.state= {
-            brandName:'My Application'
+            brandName:'User Management'
         }
     }
     render() {
+        let profileImageStyle = {backgroundImage:`url(${userImage})`};
         return (
             <div className="layout--top--navbar">
                 <div className="brand">
@@ -14,7 +16,7 @@ class TopNavBarComponent extends React.Component {
                     <span>{this.state.brandName}</span>
                 </div>
                 <div className="profile-section">
-                    <div className="profile-image">
+                    <div className="profile-image" style={profileImageStyle}>
                     </div>
                     <div className="profile-name">
                         <span className="m-1">Jane Doe</span>
