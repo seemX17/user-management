8799461bf6e44ac4858fe3c0225e282e557d277c
SeemranXec <bleh17xec@hotmail.com>
2019-11-07 12:49:09 +0530

    setting selected tab color

--- src/dashboard/sidebar/Side-nav.component.jsx
+++ src/dashboard/sidebar/Side-nav.component.jsx
@@ -5,7 +5,8 @@ class SideNavbarComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            itemList: ['Product', 'Demo Scripts', 'Customers', 'Sales Team', 'Demos', 'Settings']
+            itemList: ['Product', 'Demo Scripts', 'Customers', 'Sales Team', 'Demos', 'Settings'],
+            isActiveId: 0
         };
     }
     render() {
@@ -13,14 +14,25 @@ class SideNavbarComponent extends React.Component {
             <div className="layout--side--navbar">
                 <ul>
                     {
-                        this.state.itemList.map(function (title, index) {
-                            let routePath = `/${title.toLowerCase().replace(/\s/g,'')}`;
-                            return <li key={index}><Link to={routePath} > {title} </Link></li>
+                        this.state.itemList.map((title, index) => {
+                            let routePath = `/${title.toLowerCase().replace(/\s/g, '')}`;
+                            return <li key={index} className={(this.state.isActiveId === index) ? 'navigation--active' : ''} onClick={() => this.setActive(index)}>
+                                <Link to={routePath} >
+                                    {title}
+                                </Link>
+                            </li>
                         })
                     }
                 </ul>
             </div>
         );
+    }
+    setActive(position) {
+        if (this.state.isActiveId === position) {
+            this.setState({ isActiveId: null })
+        } else {
+            this.setState({ isActiveId: position })
+        }
     }
 }
 export default SideNavbarComponent;
